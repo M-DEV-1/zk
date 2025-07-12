994bca9f56b8e2cebbeb0a6238ffb938599666c3
hrishihunde <hrishixhrishi@gmail.com>
2025-07-13 04:04:10 +0530

    add: wagmi, wrapper, and wallet connection
    Signed-off-by: hrishihunde <hrishixhrishi@gmail.com>

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WagmiWrapper from "@/components/wrapper/wagmi";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Toaster />
+        <WagmiWrapper>{children}</WagmiWrapper>
       </body>
     </html>
   );
